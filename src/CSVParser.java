c0ddcec8b60ed0d75ebecdcaaedb559a5ad84334
davjon711 <epnotaryjones@gmail.com>
2025-04-19 19:14:42 -0600

    Add files via upload
    Updated CSV parser and Space Object class

--- src/CSVParser.java
+++ src/CSVParser.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,15 +24,14 @@ public class CSVParser {
         StringBuilder field = new StringBuilder(32);
         boolean inQuotes = false;
         
-        //We iterate over the string one character at a time
-        for (int i = 0; i < line.length(); i++) {
+        for (int i = 0; i < line.length(); i++) {   //We iterate over the string one character at a time
             char c = line.charAt(i);
 
             if (c == '\"') {                        //Enclosing quote detected
                 inQuotes = !inQuotes;               //Switches mode to include next comma in entry
             } else if (c == ',' && !inQuotes) {     //Comma outside of enclosing quotes means end of entry
                 fields.add(field.toString());       //Adds current field to list
-                field.setLength(0);                 //Resets field builder
+                field.setLength(0);       //Resets field builder
             } else {
                 field.append(c);                    //Adds current character to field string
             }
@@ -51,6 +52,7 @@ public class CSVParser {
         String sattelliteName = fields.get(2);
         String country = fields.get(3);
         String orbitType = fields.get(4);
+        String object_type = fields.get(5);
         int launchYear = Integer.parseInt(fields.get(6));
         String launchSite = fields.get(7);
         double longitude = Double.parseDouble(fields.get(8));
@@ -58,16 +60,16 @@ public class CSVParser {
         String geohash = fields.get(10);
         int daysOld = Integer.parseInt(fields.get(18));
         long conjunctionCount = Long.parseLong(fields.get(19));
-        return new SpaceObject(recordID, sattelliteName, country, orbitType,
+        return new SpaceObject(recordID, sattelliteName, country, orbitType, object_type,
                             launchYear, launchSite, longitude, averageLongitude,
                             geohash, daysOld, conjunctionCount);
     }
 
     /**
-     * Reads a CSV file line by line
+     * Reads a CSV file line by line and creates a new object from each entry
      * 
      * @param filename - The given file where the data is stored
-     * @return A list of all the entries in the file
+     * @return A list of all the created objects
      */
     public static List<SpaceObject> readCsvFile(String filename) {
         List<SpaceObject> entries = new ArrayList<>();                                 //Fields are stored as a list of Strings
@@ -84,5 +86,36 @@ public class CSVParser {
         }
 
         return entries;
+    }
+
+    public static void writeRecordsToCsv(List<SpaceObject> records, String filename){
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            // Write header
+            writer.write("record_id,norad_cat_id,satellite_name,country,approximate_orbit_type,object_type,launch_year,launch_site,longitude,avg_longitude,geohash,HRR_Category,is_nominated,nominated_at,has_dossier,last_updated_at,justification,focused_analysis,days_old,conjunction_count,is_unk_object,all_maneuvers,days_since_ob,recent_maneuvers,deltaV_90day,has_sister_debris");
+            writer.newLine();
+
+            // Write each record
+            for (SpaceObject object : records) {
+                writer.write(object.toCsv());
+                writer.newLine();
+            }
+        }catch(IOException e){
+            System.out.println(e);
+        }
+    }
+    
+    public static void main(String[] args){
+        String fileName = "rso_metrics_test1.csv";
+        long startTime = System.nanoTime();
+        List<SpaceObject> testList = readCsvFile(fileName);
+        for(SpaceObject x : testList){
+            if(x.getOrbitType().equals("LEO")){
+                System.out.println(x);
+            }
+        }
+        writeRecordsToCsv(testList, "rso_metrics_test2.csv");
+        long endTime = System.nanoTime();
+        long totalTime = (endTime - startTime) / 1000000;
+        System.out.println("Elapsed time: " + totalTime);
     }
 }
--- src/SpaceObject.java
+++ src/SpaceObject.java
@@ -1,23 +1,49 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.security.Timestamp;
+import java.util.List;
+
 public class SpaceObject{
     private String recordID;
+    private String norad_cat_id;
     private String sattelliteName;
     private String country;
     private String orbitType;
+    private String object_type;
     private int launchYear;
     private String launchSite;
     private double longitude;
     private double averageLongitude;
     private String geohash;
+    private String hrr_category = "";
+    private boolean is_nominated = false;
+    private Timestamp nominated_at = null;
+    private boolean has_dossier = false;
+    private Timestamp last_updated = null;
+    private String justification = "";
+    private String focusedAnalysis = "";
     private int daysOld;
-    private long conjunctionCount;
+    private long conjunctionCount = -1;
+    private boolean is_unk_obj;
+    private String allManeuvers = "";
+    private int days_since_ob = -1;
+    private String recentManeuvers = "";
+    private double deltaV90day = -1.0;
+    private boolean has_sister_debris = false;
+    private boolean stillInOrbit;
+    private String riskLevel;
+    
 
-    SpaceObject(String recordID, String satelliteName, String country, String orbitType,
+    SpaceObject(String recordID, String satelliteName, String country, String orbitType, String object_type,
                 int launchYear, String launchSite, double longitude, double averageLongitude,
                 String geohash, int daysOld, long conjunctionCount){
                     this.recordID = recordID;
+                    this.norad_cat_id = recordID;
                     this.sattelliteName = satelliteName;
                     this.country = country;
                     this.orbitType = orbitType;
+                    this.object_type = object_type;
                     this.launchYear = launchYear;
                     this.launchSite = launchSite;
                     this.longitude = longitude;
@@ -25,44 +51,69 @@ public class SpaceObject{
                     this.geohash = geohash;
                     this.daysOld = daysOld;
                     this.conjunctionCount = conjunctionCount;
+
+                    if(((orbitType == null) || ((longitude > 180) ||
+                         (longitude < -180)) || (daysOld >= 15000)) &&
+                          (conjunctionCount == 0)){
+                            this.stillInOrbit = false;
+                    }else{
+                        this.stillInOrbit = true;   
+                    }
+
+                    double orbitalDrift = Math.abs((longitude - averageLongitude));
+                    if(orbitalDrift >= 50){
+                        this.riskLevel = "High";
+                        }
+                    else if(orbitalDrift < 50 && orbitalDrift >= 10){
+                        this.riskLevel = "Moderate";
+                        }
+                    else{
+                        this.riskLevel = "Low";
+                    }
+
+                    if(!object_type.equals("Unknown")){
+                        this.is_unk_obj = false;
+                    }else{
+                        this.is_unk_obj = true;
+                    }
                 }
 
-    public String getRecordID() {
-        return recordID;
-    }
-    public String getSattelliteName() {
-        return sattelliteName;
-    }
-    public String getCountry() {
-        return country;
-    }
     public String getOrbitType() {
         return orbitType;
-    }
-    public int getLaunchYear() {
-        return launchYear;
-    }
-    public String getLaunchSite() {
-        return launchSite;
-    }
-    public double getLongitude() {
-        return longitude;
-    }
-    public double getAverageLongitude() {
-        return averageLongitude;
-    }
-    public String getGeohash() {
-        return geohash;
-    }
-    public int getDaysOld() {
-        return daysOld;
-    }
-    public long getConjunctionCount() {
-        return conjunctionCount;
     }
 
     @Override
     public String toString() {
-        return "RecordID: " + recordID + " Launch Year: " + launchYear + " Launch Site: " + launchSite;
+        return "  " + recordID + "  " + norad_cat_id +  "  " + sattelliteName + "  " +
+                country + "  " + orbitType + "  " + object_type + "  " + launchYear + "  " + 
+                launchSite + "  " + longitude + "  " + averageLongitude + "  " +
+                geohash + "  " + hrr_category + "  " + is_nominated + "  " + nominated_at + "  " +
+                has_dossier + "  " + last_updated + "  " + justification + "  " + focusedAnalysis + "  " +
+                daysOld + "  " + conjunctionCount + "  " + is_unk_obj + "  " + allManeuvers + "  " +
+                days_since_ob + "  " + recentManeuvers + "  " + deltaV90day + "  " + has_sister_debris + "  " +
+                stillInOrbit + "  " + riskLevel;
+    }
+
+    public String toCsv(){
+        return String.format("%s,%s,%s,%s,%s,%s,%s,%s,%.8f,%.8f,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s",
+                            recordID, norad_cat_id, sattelliteName, country, orbitType, object_type,
+                            launchYear, launchSite, longitude, averageLongitude, geohash, hrr_category,
+                            is_nominated, nominated_at, has_dossier, last_updated, justification,
+                            focusedAnalysis, daysOld, conjunctionCount, is_unk_obj, allManeuvers, days_since_ob,
+                            recentManeuvers, deltaV90day, has_sister_debris, stillInOrbit, riskLevel);
+    }
+
+    public void writeRecordsToCsv(List<SpaceObject> records, String filename) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            // Write header
+            writer.write("record_id,norad_cat_id,satellite_name,country,approximate_orbit_type,object_type,launch_year,launch_site,longitude,avg_longitude,geohash,HRR_Category,is_nominated,nominated_at,has_dossier,last_updated_at,justification,focused_analysis,days_old,conjunction_count,is_unk_object,all_maneuvers,days_since_ob,recent_maneuvers,deltaV_90day,has_sister_debris");
+            writer.newLine();
+
+            // Write each record
+            for (SpaceObject object : records) {
+                writer.write(object.toCsv());
+                writer.newLine();
+            }
+        }
     }
 }
