76c96de9e8b529dc41cf462600fdb150c64dfcb4
Fabian Ornelas <fabe.ornelas98@gmail.com>
2025-05-06 22:26:23 -0600

    Backed up and deleted deprecated classes: PL, PM, UF

--- src/PromptLogin.java
+++
@@ -1,7 +0,0 @@
-/** 
- * This class prompts all users to login to the system.
- * Once a user selects their role, a prompt for username and password shall be given.
- * 
- * This class is a placeholder and does not yet perform any actual login functionality.
- */
-class PromptLogin {}
